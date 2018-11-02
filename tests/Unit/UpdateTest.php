680d919affce4d016fbe6fb06c42f255d6fb8169
karthikreddy <ka452@njit.edu>
2018-11-02 13:59:20 -0400

    UpdateTest.php created

---
+++ tests/Unit/UpdateTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Car;
+
+class UserUpdateTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testUpdateUser()
+    {
+        $user = User::find(5);
+        $user->name = 'Steve Smith';
+        $user->save();
+        $this->assertDatabaseHas('users', ["name" => "Steve Smith"]);
+
+    }
+}
