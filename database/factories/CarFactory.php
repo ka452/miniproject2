86acee054700b94f0e0cab91416856404c7e12f0
karthikreddy <ka452@njit.edu>
2018-11-02 11:45:24 -0400

    Created a factory for cars that sets the make field to either ford / honda / toyota.

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\car::class, function (Faker $faker) {
+    $random=mt_rand(0,2);
+    $CarsArray = array("Ford", "Honda", "Toyota");
+    return [
+        'make' => $CarsArray[$random],
+        'model' => $faker->company,
+        'year' => $faker->year($max = 'now'),
+    ];
+});
